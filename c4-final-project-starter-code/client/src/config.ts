8aac8dbb83024ee701af93fe9a1033e78a4d5f7f
Zubair Riaz <zubairriaz78696@gmail.com>
2020-05-07 06:46:09 +0500

    update config.ts

--- c4-final-project-starter-code/client/src/config.ts
+++ c4-final-project-starter-code/client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-srbdleo3.auth0.com',            // Auth0 domain
+  clientId: 'cKgM5WSQ0ncFfe5GehOj1NqL7An7J95c',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
